1996c39121e85b4f62635966f66d8048ab830d56
fxricky <fanxuricky@gmail.com>
2023-05-01 17:11:34 +0800

    feat: add user to leaderboard

--- src/pages/LeaderboardPage/index.tsx
+++ src/pages/LeaderboardPage/index.tsx
@@ -14,12 +14,46 @@ import Colors from '../../theme/Colors';
 import {LeaderboardInfo} from '../../type';
 import LeaderboardListItem from './components/LeaderboardListItem';
 import Seperator from './components/Seperator';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {STORAGE_KEYS} from '../../constants';
 
 type Props = {};
 
 const DATA = leaderboard;
 
 const LeaderboardPage: React.FC<Props> = ({}) => {
+  const [leaderboardData, setLeaderboardData] = React.useState<
+    LeaderboardInfo[]
+  >([]);
+
+  React.useEffect(() => {
+    (async () => {
+      const userScore = +(
+        (await AsyncStorage.getItem(STORAGE_KEYS.USER_SCORE)) || ''
+      );
+
+      const index = DATA.findIndex(d => d.score <= userScore);
+
+      const updatedLeaderboard = [...DATA];
+
+      const currentUserData = {
+        id: Date.now(),
+        name: 'You',
+        score: userScore,
+      };
+
+      if (index < 0) {
+        updatedLeaderboard.push(currentUserData);
+      } else {
+        updatedLeaderboard.splice(index, 0, currentUserData);
+      }
+
+      console.log(updatedLeaderboard);
+
+      setLeaderboardData(updatedLeaderboard);
+    })();
+  }, []);
+
   const renderLeaderboardList: ListRenderItem<LeaderboardInfo> = ({item}) => {
     return <LeaderboardListItem data={item} />;
   };
@@ -33,29 +67,35 @@ const LeaderboardPage: React.FC<Props> = ({}) => {
             source={{uri: 'https://picsum.photos/200'}}
             style={styles.top3ProfilePic}
           />
-          <Text style={styles.top3InfoNameTxt}>{DATA[1].name}</Text>
-          <Text style={styles.top3InfoScoreTxt}>{DATA[1].score}</Text>
+          <Text style={styles.top3InfoNameTxt}>{leaderboardData[1]?.name}</Text>
+          <Text style={styles.top3InfoScoreTxt}>
+            {leaderboardData[1]?.score}
+          </Text>
         </View>
         <View style={styles.top3InfoContainer}>
           <Image
             source={{uri: 'https://picsum.photos/200'}}
             style={styles.top3ProfilePic}
           />
-          <Text style={styles.top3InfoNameTxt}>{DATA[0].name}</Text>
-          <Text style={styles.top3InfoScoreTxt}>{DATA[0].score}</Text>
+          <Text style={styles.top3InfoNameTxt}>{leaderboardData[0]?.name}</Text>
+          <Text style={styles.top3InfoScoreTxt}>
+            {leaderboardData[0]?.score}
+          </Text>
         </View>
         <View style={[styles.top3InfoContainer, {justifyContent: 'flex-end'}]}>
           <Image
             source={{uri: 'https://picsum.photos/200'}}
             style={styles.top3ProfilePic}
           />
-          <Text style={styles.top3InfoNameTxt}>{DATA[2].name}</Text>
-          <Text style={styles.top3InfoScoreTxt}>{DATA[2].score}</Text>
+          <Text style={styles.top3InfoNameTxt}>{leaderboardData[2]?.name}</Text>
+          <Text style={styles.top3InfoScoreTxt}>
+            {leaderboardData[2]?.score}
+          </Text>
         </View>
       </View>
       <View style={{flex: 3}}>
         <FlatList
-          data={DATA.slice(3)}
+          data={leaderboardData.slice(3)}
           renderItem={renderLeaderboardList}
           contentContainerStyle={styles.listContainer}
           ItemSeparatorComponent={() => <Seperator />}
